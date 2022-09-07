dcfa68b0f2b87f91276d47c747895f602d5d3648
esadakman <esadd26@gmail.com>
2022-09-07 12:17:51 +0300

    users html's views. register

--- main/settings.py
+++ main/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -109,6 +110,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+MEDIA_URL="media/"
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("todo.urls") ), 
+    path('', include("todo.urls") ),
     path('accounts/',include("django.contrib.auth.urls")),
     path('users/',include('users.urls')),
 ]
