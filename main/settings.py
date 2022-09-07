d750a43f47eaba559804445fbdef2434bbc3cb18
esadakman <esadd26@gmail.com>
2022-09-07 13:03:40 +0300

    crispy_forms

--- main/settings.py
+++ main/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'todo',
     'users',
 ]
@@ -115,6 +116,7 @@ LOGOUT_REDIRECT_URL = 'home'
 MEDIA_URL="media/"
 MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
